62366f4febeaf3b7a4d1cde383c18a0bd7d89843
faisalill <mfaisal.ilyas.d@gmail.com>
2023-04-23 00:25:19 +0530

    demo notes directory

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("/isagi.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Coming Soon...</h1>
+            <p className="mb-5">
+            Notes || Completed Assignment || Records
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default index
+export default index;
